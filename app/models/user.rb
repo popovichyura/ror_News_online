efe399fefa459a8d997e6ae5f11fca527c14349e
popovichyura <yurapopovich17@gmail.com>
2018-01-27 13:08:26 +0200

    add avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
 
   has_many :articles
   has_many :article_comments
+
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>"}, default_url: "/images/default.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
